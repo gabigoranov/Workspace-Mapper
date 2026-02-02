3cc3d2f46aada99ad60b3318d86e0e39b626ada3
gabigoranov <gabigoranov@abv.bg>
2026-02-02 22:06:02 +0200

    Implement sidebar item selection, overwrite FluentTheme default styles.

--- ViewModels/SidebarViewModel.cs
+++ ViewModels/SidebarViewModel.cs
@@ -9,6 +9,11 @@ public partial class SidebarViewModel(INavigationService navigation, IStartupSer
 {
     [ObservableProperty]
     private bool _isStartupApp = startup.IsEnabled();
+
+    [ObservableProperty] 
+    [NotifyPropertyChangedFor(nameof(IsHomeSelected))]
+    [NotifyPropertyChangedFor(nameof(IsSettingsSelected))]
+    private string _selectedNavItem = "Home";
     
     partial void OnIsStartupAppChanged(bool value)
     {
@@ -17,12 +22,24 @@ public partial class SidebarViewModel(INavigationService navigation, IStartupSer
         else
             startup.Disable();
     }
-    
+
     [RelayCommand]
     private void GoHome()
-        => navigation.Navigate<HomeViewModel>();
+    {
+        SelectedNavItem = "Home";
+        navigation.Navigate<HomeViewModel>();
+    }
 
     [RelayCommand]
     private void GoCreateWorkflow()
         => navigation.Navigate<CreateWorkflowViewModel>();
+
+    [RelayCommand]
+    private void GoSettings()
+    {
+        SelectedNavItem = "Settings";
+    }
+    
+    public bool IsHomeSelected => SelectedNavItem == "Home";
+    public bool IsSettingsSelected => SelectedNavItem == "Settings";
 }
